da5d79ed599a75ac3bc9c8ffaed57a59cf71e290
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2020-09-30 16:11:30 -0300

    Creacion de Namespaces y Escenas de Testeo

---
+++ Assets/- Diego/Scripts/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSVJ1.Diego
+{
+    public class CharacterController : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            transform.position += transform.right * (speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/_EjemploNamespace_Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSVJ1.Main
+{
+    public class _EjemploNamespace_Main : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
