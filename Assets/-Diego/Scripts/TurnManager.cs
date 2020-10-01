16eb2b177485cc6084e28d941d30f1fa44f9fc4c
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2020-10-01 03:04:42 -0300

    TurnManager
    Ya está casi listo, solo faltaría la parte del disparo y analizar si hace falta un par de rondas de solo movimiento.

--- Assets/-Diego/Scripts/TurnManager.cs
+++ Assets/-Diego/Scripts/TurnManager.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MSVJ1.Diego
 {
     public class TurnManager : MonoBehaviour
     {
-        [Header("Players")]
+        [Header("Players Settings")]
         [SerializeField] private GameObject prefabPlayer1 = null;
         private CharacterController controllerPlayer1 = null;
         [SerializeField] private GameObject prefabPlayer2 = null;
         private CharacterController controllerPlayer2 = null;
 
-        [Header("Turns")]
-        [SerializeField] private float turnDuration = 0f;
-        private float timer = 0f;
-        private bool doOnce = true;
+        [Header("Turns Settings")]
         private int currentTurn;
+        [SerializeField] private float startDuration = 0f;
+        private float startTimer = 0f;
+        private bool canStart = true;
+        [SerializeField] private float movementDuration = 0f;
+        private float movementTimer = 0f;
+        private bool canMove = false;
+        private bool canShoot = false;
+
+        [Header("Canvas Settings")]
+        [SerializeField] private Text textText = null;
+        [SerializeField] private Text textNumber = null;
 
 
         private void Start()
@@ -24,66 +33,81 @@ namespace MSVJ1.Diego
             controllerPlayer1 = prefabPlayer1.GetComponent<CharacterController>();
             controllerPlayer2 = prefabPlayer2.GetComponent<CharacterController>();
 
+            controllerPlayer1.enabled = false;
+            controllerPlayer2.enabled = false;
+
             currentTurn = (int)Random.Range(1, 3);
-            Debug.Log("Empieza jugando Player" + currentTurn);
 
-            if (currentTurn == 1)
-            {
-                EnableController(controllerPlayer1);
-                DisableController(controllerPlayer2);
-            }
-            else if (currentTurn == 2)
-            {
-                EnableController(controllerPlayer2);
-                DisableController(controllerPlayer1);
-            }
-
-            ResetTimer();
+            startTimer = startDuration;
+            movementTimer = movementDuration;
         }
 
         private void Update()
         {
-            if (timer >= turnDuration && doOnce)
+            // Start Turn
+            if (startTimer <= 0 && canStart)
             {
-                if (currentTurn == 1)
-                {
-                    EnableController(controllerPlayer2);
-                    DisableController(controllerPlayer1);
-                    currentTurn = 2;
-                }
-                else if (currentTurn == 2)
-                {
-                    EnableController(controllerPlayer1);
-                    DisableController(controllerPlayer2);
-                    currentTurn = 1;
-                }
+                canStart = false;
 
-                doOnce = false;
+                textText.text = "";
+                textNumber.text = "";
 
-                Debug.Log("Comienza el turno de Player" + currentTurn);
+                if (currentTurn == 1) controllerPlayer1.enabled = true;
+                else if (currentTurn == 2) controllerPlayer2.enabled = true;
 
-                ResetTimer();
+                canMove = true;
             }
-            else
+            else if (startTimer > 0 && canStart)
             {
-                timer += Time.deltaTime;
+                startTimer -= Time.deltaTime;
+                textText.text = "Turno de Jugador" + currentTurn + " comienza en";
+                int intStartTimer = (int)startTimer + 1;
+                textNumber.text = intStartTimer.ToString();
             }
-        }
 
-        private void ResetTimer()
-        {
-            timer = 0;
-            doOnce = true;
-        }
+            // Movement
+            if (movementTimer <= 0 && canMove)
+            {
+                canMove = false;
 
-        private void DisableController (CharacterController controller)
-        {
-            controller.enabled = false;
-        }
+                textText.text = "";
+                textNumber.text = "";
 
-        private void EnableController(CharacterController controller)
-        {
-            controller.enabled = true;
+                if (currentTurn == 1) controllerPlayer1.enabled = false;
+                else if (currentTurn == 2) controllerPlayer2.enabled = false;
+
+                canShoot = true;
+            }
+            else if (movementTimer > 0 && canMove)
+            {
+                movementTimer -= Time.deltaTime;
+                textText.text = "Jugador" + currentTurn + " puede moverse!";
+                int intMovementTimer = (int)movementTimer + 1;
+                textNumber.text = intMovementTimer.ToString();
+            }
+
+            // Shooting
+            if (canShoot && Input.GetKeyDown(KeyCode.Space))
+            {
+                canShoot = false;
+
+                textText.text = "";
+            }
+            else if(canShoot)
+            {
+                textText.text = "Jugador" + currentTurn + " puede DISPARAR!";
+            }
+
+            // Finish Turn
+            if (!canStart && !canMove && !canShoot)
+            {
+                currentTurn = currentTurn == 1 ? 2 : 1;
+
+                startTimer = startDuration;
+                movementTimer = movementDuration;
+
+                canStart = true;
+            }
         }
     }
 }
