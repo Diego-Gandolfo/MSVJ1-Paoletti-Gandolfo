12ffebac0a1c2113ef102b33e9445236409b5997
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2020-10-12 21:00:07 -0300

    Cosillas pequeñas
    - Creación de Carpetas de Prefabs Diego
    - Organizacion de Prefabs Diego
    - Script AutoDestroy Diego

---
+++ Assets/-Diego/Scripts/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSVJ1.Diego
+{
+    public class AutoDestroy : MonoBehaviour
+    {
+        [SerializeField] private float timeToDestroy = 600f;
+
+        private void Start()
+        {
+            Destroy(gameObject, timeToDestroy);
+        }
+    }
+}
