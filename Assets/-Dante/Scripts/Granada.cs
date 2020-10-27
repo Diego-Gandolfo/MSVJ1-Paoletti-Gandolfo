507cdd7998a86ac0d6643907a6814bd1f74b2649
Dante <dantedibujante@gmail.com>
2020-10-27 06:05:27 -0300

    Nuevo scripts y sprites
    Sprites de vigas para que rebote la bala o granada, unas cajas, y un piso.
    
    scripts de daño en area de la granada, lifecontroller y parallax

---
+++ Assets/-Dante/Scripts/Granada.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSVJ1.Dante
+{
+public class Granada : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float explosionTime;
+    [SerializeField] private float explosionRadius;
+    [SerializeField] private float explosionIntensity;
+     [SerializeField] private LayerMask layerMask;
+    
+
+    
+
+
+    private float currentExplosionTime;
+        void Update()
+        {
+            currentExplosionTime += Time.deltaTime;
+
+            if(currentExplosionTime >= explosionTime)
+            {
+                Explosion();
+            }
+        }
+
+        private void Explosion()
+        {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll((Vector2)transform.position, explosionRadius, layerMask);
+
+        foreach (var collider in colliders)
+        {
+            Rigidbody2D rigidbody = collider.gameObject.GetComponent<Rigidbody2D>();
+                if(rigidbody != null)
+                {
+                    Vector3 direction = collider.transform.position - transform.position;
+                    float distance = direction.magnitude;
+                    direction.Normalize();
+                    //Mientras mas lejos estan de la explosion, menos  impulso reciben...
+                    rigidbody.AddForce((direction * explosionIntensity) / distance, ForceMode2D.Impulse);
+                    Destroy(gameObject);
+                }
+                LifeController lifeController = collider.GetComponent<LifeController>();
+                if(lifeController != null)
+                {
+                    lifeController.GetDamage((damage * (Vector3.Distance(collider.transform.position, transform.position))) / explosionRadius );
+                    //lifeController.GetDamage(damage / (Vector3.Distance ( collider.transform.position, transform.position)));
+                }
+        }    
+
+        }
+}
+}
---
+++ Assets/-Dante/Scripts/LifeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MSVJ1.Dante
+{
+public class LifeController : MonoBehaviour
+{
+    
+        [SerializeField] private float maxLife = 100;
+        [SerializeField] private float currentLife;
+
+        
+        
+
+        private void Awake()
+        {
+            
+            currentLife = maxLife;
+        }
+
+        public void ResetLife()
+        {
+            currentLife = maxLife;
+        }
+
+        public void GetDamage(float damage)
+        {
+            currentLife -= damage;
+
+            
+
+            if (currentLife <= 0)
+            {
+                
+                Invoke("Kill", 0);
+            }
+        }
+        
+        public void GetHeal (float heal)
+        {
+            if (currentLife < maxLife)
+            {
+                currentLife += heal;
+
+                if (currentLife > maxLife)
+                {
+                    currentLife = maxLife;
+                }
+            }
+        }
+
+        private void Kill()
+        {
+            Destroy(gameObject);
+        }
+
+        public float GetCurrentLife()
+        {
+            return currentLife;
+        }
+}
+}
